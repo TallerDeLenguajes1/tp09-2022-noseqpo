598fdd3e56ade8de6d1cb2d061b6489fbaac41bd
Daniel Paz <danielnicolaspeace@gmail.com>
2022-07-18 17:50:16 -0300

    push

---
+++ TP9/TP9/Producto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP9
+{
+    class Producto
+    {
+        private string nombre;
+        private DateTime fechaVencimiento;
+        private float precio;
+        private string tam;
+
+        public Producto()
+        {
+            string[] listaNombres = { "Yogurt", "Fideos", "Manteca", "Atun", "Mermelada", "Leche", "Gaseosa", "Galleta", "Pan", "Embutido" };
+            string[] listaTam = { "Pequeño", "Mediano", "Grande" };
+            Random rnd = new Random();
+
+            this.nombre = listaNombres[rnd.Next(0, 10)];
+            this.fechaVencimiento = new DateTime();
+            this.fechaVencimiento = fechaVencimiento.AddDays(rnd.Next(0, 31));
+            this.fechaVencimiento = fechaVencimiento.AddMonths(rnd.Next(0, 13));
+            this.fechaVencimiento = fechaVencimiento.AddYears(rnd.Next(2022, 2025));
+            this.precio = rnd.Next(1000, 5000);
+            this.tam = listaTam[rnd.Next(0, 3)];
+        }
+
+        public override string ToString()
+        {
+            string cadena = "";
+
+            cadena += $"Nombre: {this.nombre}\n";
+            cadena += $"Fecha de vencimiento: {this.fechaVencimiento.ToString("dd/MM/yyyy")}\n";
+            cadena += $"Precio: {this.precio}\n";
+            cadena += $"Tamaño: {this.tam}";
+
+            return cadena;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public DateTime FechaVencimiento { get => fechaVencimiento; set => fechaVencimiento = value; }
+        public float Precio { get => precio; set => precio = value; }
+        public string Tam { get => tam; set => tam = value; }
+    }
+}
---
+++ TP9/TP9/Program.cs
@@ -0,0 +1,34 @@
+﻿using TP9;
+using System.Text.Json;
+
+string path;
+int cantidad;
+List<Producto> listaProductos = new List<Producto>();
+
+Console.Write("Ruta: ");
+path = Console.ReadLine();
+
+if (!File.Exists(path + @"/index.json"))
+{
+    File.Create(path + @"/index.json").Close();
+}
+
+path += @"/index.json";
+
+Console.Write("Ingresar la cantidad de productos a generar: ");
+cantidad = Int32.Parse(Console.ReadLine());
+
+for (int i = 0; i < cantidad; i++)
+{
+    listaProductos.Add(new Producto());
+}
+
+string archivoJson = JsonSerializer.Serialize(listaProductos);
+File.WriteAllText(path, archivoJson);
+
+listaProductos = JsonSerializer.Deserialize<List<Producto>>(File.ReadAllText(path));
+
+foreach (Producto prod in listaProductos)
+{
+    Console.WriteLine(prod + "\n");
+}
